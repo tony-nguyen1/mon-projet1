f06af4f37e26a5fd3e3e34af849363782edf5b6f
tony-nguyen1 <nguyentoony@gmail.com>
2022-12-11 15:40:41 +0100

    First commit

---
+++ src/main/java/um/fds/agl/Main.java
@@ -0,0 +1,7 @@
+package um.fds.agl;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/test/java/um/fds/agl/TestMain.java
@@ -0,0 +1,10 @@
+package um.fds.agl;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class TestMain {
+    @Test
+    void testApp() { assertTrue( true ); }
+}
